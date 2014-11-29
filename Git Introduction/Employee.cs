a1e5e3bd128ef35b50858e6f13790ff881f09292
BBB <beisenov_b_b@mail.ru>
2014-11-29 20:29:21 +0300

    Added Employee class

---
+++ Git Introduction/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Introduction
+{
+    class Employee
+    {
+        public int Id { get; set; }
+    }
+}
